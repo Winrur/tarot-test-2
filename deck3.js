abc56d1713b59e669cc2fea740319a1a35c73990
Winrur <35635213+Winrur@users.noreply.github.com>
2018-02-19 16:44:45 +0300

    Add files via upload

---
+++ deck3.js
@@ -0,0 +1,626 @@
+var tarotDeck = {
+
+    "1": {
+
+        "card-meaning": "New beginnings, optimism, trust in life.",
+
+        "card-name": "The Fool"
+
+    },
+
+    "2": {
+
+        "card-meaning": "Action, the power to manifest.",
+
+        "card-name": "The Magician"
+
+    },
+
+    "3": {
+
+        "card-meaning": "Inaction, going within, the subconscious.",
+
+        "card-name": "The High Priestess"
+
+    },
+
+    "4": {
+
+        "card-meaning": "Abundance, nurturing, fertility, life in bloom.",
+
+        "card-name": "The Empress"
+
+    },
+
+    "5": {
+
+        "card-meaning": "Structure, stability, rules and power.",
+
+        "card-name": "The Emperor"
+
+    },
+
+    "6": {
+
+        "card-meaning": "Institutions, tradition, society and its rules.",
+
+        "card-name": "The Hierophant"
+
+    },
+
+    "7": {
+
+        "card-meaning": "Passion, choice, uniting.",
+
+        "card-name": "The Lovers"
+
+    },
+
+    "8": {
+
+        "card-meaning": "Movement, progress, integration.",
+
+        "card-name": "The Chariot"
+
+    },
+
+    "9": {
+
+        "card-meaning": "Courage, subtle power, integration of animal self.",
+
+        "card-name": "Strength"
+
+    },
+
+    "10": {
+
+        "card-meaning": "Meditation, solitude, consciousness.",
+
+        "card-name": "The Hermit"
+
+    },
+
+    "11": {
+
+        "card-meaning": "Cycles, change, ups and downs.",
+
+        "card-name": "Wheel of Fortune"
+
+    },
+
+    "12": {
+
+        "card-meaning": "Fairness, equality, balance.",
+
+        "card-name": "Justice"
+
+    },
+
+    "13": {
+
+        "card-meaning": "Surrender, new perspective, enlightenment.",
+
+        "card-name": "The Hanged Man"
+
+    },
+
+    "14": {
+
+        "card-meaning": "The end of something, change, the impermeability of all things.",
+
+        "card-name": "Death"
+
+    },
+
+    "15": {
+
+        "card-meaning": "Balance, moderation, being sensible.",
+
+        "card-name": "Temperance"
+
+    },
+
+    "16": {
+
+        "card-meaning": "Destructive patterns, addiction, giving away your power.",
+
+        "card-name": "The Devil"
+
+    },
+
+    "17": {
+
+        "card-meaning": "Collapse of stable structures, release, sudden insight.",
+
+        "card-name": "The Tower"
+
+    },
+
+    "18": {
+
+        "card-meaning": "Hope, calm, a good omen!",
+
+        "card-name": "The Star"
+
+    },
+
+    "19": {
+
+        "card-meaning": "Mystery, the subconscious, dreams.",
+
+        "card-name": "The Moon"
+
+    },
+
+    "20": {
+
+        "card-meaning": "Success, happiness, all will be well!",
+
+        "card-name": "The Sun"
+
+    },
+
+    "21": {
+
+        "card-meaning": "Rebirth, a new phase, inner calling.",
+
+        "card-name": "Judgement"
+
+    },
+
+    "22": {
+
+        "card-meaning": "Completion, wholeness, attainment, celebration of life.",
+
+        "card-name": "The World"
+
+    },
+
+    "23": {
+
+        "card-meaning": "New beginnings, creative spark, fertile ideas.",
+
+        "card-name": "Ace of Wands"
+
+    },
+
+    "24": {
+
+        "card-meaning": "Contemplation, assessing ones life direction.",
+
+        "card-name": "Two of Wands"
+
+    },
+
+    "25": {
+
+        "card-meaning": "Reaping the rewards of your efforts.",
+
+        "card-name": "Three of Wands"
+
+    },
+
+    "26": {
+
+        "card-meaning": "Celebration, safety, the home.",
+
+        "card-name": "Four of Wands"
+
+    },
+
+    "27": {
+
+        "card-meaning": "Competition, minor struggles or disagreements.",
+
+        "card-name": "Five of Wands"
+
+    },
+
+    "28": {
+
+        "card-meaning": "Success, accolades and achievement.",
+
+        "card-name": "Six of Wands"
+
+    },
+
+    "29": {
+
+        "card-meaning": "Feeling defensive and on guard.",
+
+        "card-name": "Seven of Wands"
+
+    },
+
+    "30": {
+
+        "card-meaning": "Speed, things manifesting quickly.",
+
+        "card-name": "Eight of Wands"
+
+    },
+
+    "31": {
+
+        "card-meaning": "Pessimism, gearing up for the worst.",
+
+        "card-name": "Nine of Wands"
+
+    },
+
+    "32": {
+
+        "card-meaning": "Feeling oppressed, exhaustion, too many responsibilities.",
+
+        "card-name": "Ten of Wands"
+
+    },
+
+    "33": {
+
+        "card-meaning": "Newly inspired, excited about life and work",
+
+        "card-name": "Page of Wands"
+
+    },
+
+    "34": {
+
+        "card-meaning": "An adventurous risk taker who follows his passions.",
+
+        "card-name": "Knight of Wands"
+
+    },
+
+    "35": {
+
+        "card-meaning": "Confident, focused, has zest for life.",
+
+        "card-name": "Queen of Wands"
+
+    },
+
+    "36": {
+
+        "card-meaning": "Career focused, mature, passionate.",
+
+        "card-name": "King of Wands"
+
+    },
+
+    "37": {
+
+        "card-meaning": "Financial reward, clarity of life purpose, goals.",
+
+        "card-name": "Ace of Pentacles"
+
+    },
+
+    "38": {
+
+        "card-meaning": "Balance, multitasking",
+
+        "card-name": "Two of Pentacles"
+
+    },
+
+    "39": {
+
+        "card-meaning": "Meaningful work, enjoying one’s work, suitable career",
+
+        "card-name": "Three of Pentacles"
+
+    },
+
+    "40": {
+
+        "card-meaning": "Hoarding, feeling poor, holding self back out of fear",
+
+        "card-name": "Four of Pentacles"
+
+    },
+
+    "41": {
+
+        "card-meaning": "Minor money troubles, health problems, feeling like an outsider",
+
+        "card-name": "Five of Pentacles"
+
+    },
+
+    "42": {
+
+        "card-meaning": "Charity, accepting and giving help",
+
+        "card-name": "Six of Pentacles"
+
+    },
+
+    "43": {
+
+        "card-meaning": "Patience, waiting for your plans to bear fruit",
+
+        "card-name": "Seven of Pentacles"
+
+    },
+
+    "44": {
+
+        "card-meaning": "Hard work, focused efforts, laying the groundwork.",
+
+        "card-name": "Eight of Pentacles"
+
+    },
+
+    "45": {
+
+        "card-meaning": "Luxury, rest, financial and material comforts.",
+
+        "card-name": "Nine of Wands"
+
+    },
+
+    "46": {
+
+        "card-meaning": "Financial success, strong business relationships. ",
+
+        "card-name": "Ten of Pentacles"
+
+    },
+
+    "47": {
+
+        "card-meaning": "Student, commitment to learning.",
+
+        "card-name": "Page of Pentacles"
+
+    },
+
+    "48": {
+
+        "card-meaning": "Cautious, sensible and slow to progress.",
+
+        "card-name": "Knight of Pentacles"
+
+    },
+
+    "49": {
+
+        "card-meaning": "Healthy in body and finances, grounded and calm.",
+
+        "card-name": "Queen of Pentacles"
+
+    },
+
+    "50": {
+
+        "card-meaning": "Enjoys the good life (food, drink and leisure), financially secure",
+
+        "card-name": "King of Pentacles"
+
+    },
+
+    "51": {
+
+        "card-meaning": "Emotional fulfillment, joy",
+
+        "card-name": "Ace of Cups"
+
+    },
+
+    "52": {
+
+        "card-meaning": "Partnership, mutual attraction, compatibility",
+
+        "card-name": "Two of Cups"
+
+    },
+
+    "53": {
+
+        "card-meaning": "Celebration, fun with friends, laughter",
+
+        "card-name": "Three of Cups"
+
+    },
+
+    "54": {
+
+        "card-meaning": "Boredom, dissatisfaction with what is being offered",
+
+        "card-name": "Four of Cups"
+
+    },
+
+    "55": {
+
+        "card-meaning": "Dwelling on the negative, self pity",
+
+        "card-name": "Five of Cups"
+
+    },
+
+    "56": {
+
+        "card-meaning": "Sentimentality, kindness, help",
+
+        "card-name": "Six of Cups"
+
+    },
+
+    "57": {
+
+        "card-meaning": "So many choices! Indecision, getting lost in fantasy",
+
+        "card-name": "Seven of Cups"
+
+    },
+
+    "58": {
+
+        "card-meaning": "Abandoning something in search of something better",
+
+        "card-name": "Eight of Cups"
+    },
+
+    "59": {
+
+        "card-meaning": "Indulgence, self-satisfaction",
+
+        "card-name": "Nine of Cups"
+
+    },
+
+    "60": {
+
+        "card-meaning": "Emotional bliss, happiness, attainment ",
+
+        "card-name": "Ten of Cups"
+
+    },
+
+    "61": {
+
+        "card-meaning": "Creative, inspired, learning artistic skill",
+
+        "card-name": "Page of Cups"
+
+    },
+
+    "62": {
+
+        "card-meaning": "Romantic, adventurous, following one’s heart",
+
+        "card-name": "Knight of Cups"
+
+    },
+
+    "63": {
+
+        "card-meaning": "Emotionally nurturing, intuitive, sensitive",
+
+        "card-name": "Queen of Cups"
+
+    },
+
+    "64": {
+
+        "card-meaning": "Repression of deep feelings, possible alcoholism",
+
+        "card-name": "King of Cups"
+
+    },
+
+    "65": {
+
+        "card-meaning": "A fresh start, a sudden opportunity or idea, clarity",
+
+        "card-name": "Ace of Swords"
+
+    },
+
+    "66": {
+
+        "card-meaning": "Indecision",
+
+        "card-name": "Two of Swords"
+
+    },
+
+    "67": {
+
+        "card-meaning": "Heartbreak, betrayal",
+
+        "card-name": "Three of Swords"
+
+    },
+
+    "68": {
+
+        "card-meaning": "Meditation, rest, retreat",
+
+        "card-name": "Four of Swords"
+
+    },
+
+    "69": {
+
+        "card-meaning": "Mind games, hostility",
+
+        "card-name": "Five of Swords"
+
+    },
+
+    "70": {
+
+        "card-meaning": "Leaving, accepting help, going somewhere better",
+
+        "card-name": "Six of Swords"
+
+    },
+
+    "71": {
+
+        "card-meaning": "Secret plans, abandoning ship",
+
+        "card-name": "Seven of Swords"
+
+    },
+
+    "72": {
+
+        "card-meaning": "Feeling powerless and stuck",
+
+        "card-name": "Eight of Swords"
+
+    },
+
+    "73": {
+
+        "card-meaning": "Overactive mind, anxiety",
+
+        "card-name": "Nine of Swords"
+
+    },
+
+    "74": {
+
+        "card-meaning": "Feeling defeated, self sabotage",
+
+        "card-name": "Ten of Swords"
+
+    },
+
+    "75": {
+
+        "card-meaning": "Mentally unstable or intellectually immature, acts without thinking",
+
+        "card-name": "Page of Swords"
+
+    },
+
+    "76": {
+
+        "card-meaning": "Fierce, determined, aggressively pursues goals",
+
+        "card-name": "Knight of Swords"
+
+    },
+
+    "77": {
+
+        "card-meaning": "Intelligent, writer, communicative yet cold",
+
+        "card-name": "Queen of Swords"
+
+    },
+
+    "78": {
+
+        "card-meaning": "Serious, controlling, rational and mind/intellect-focused",
+
+        "card-name": "King of Wands"
+
+    }
+
+}
